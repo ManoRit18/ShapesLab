1257b5f9fce2d6d3860acc6b702edd912afd2937
ManoRit18 <32560046+ManoRit18@users.noreply.github.com>
2018-06-26 22:18:11 -0700

    Ok cool you made a change don't freak out

--- Square.java
+++ Square.java
@@ -1,11 +1,17 @@
 public class Square extends Quadrilateral {
+
    public Square(double side) {
       super(side, side);
    }
    public String toString(){
+      String change = "OH GOD SHE MADE A CHANGE"
       return "Square" + "    perimeter: " + super.getPerimeter() + "    area: " + super.getArea();
    }
-   public boolean equals(Object other){
+
+
+
+
+         public boolean equals(Object other){
       if(other instanceof Square){
          Square otherSquare = (Square) other;
          return this.getArea() == otherSquare.getArea();
